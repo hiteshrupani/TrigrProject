b65aa98f23736ca7f0e2c2644728d4c629d5bfc4
Hitesh Rupani <rupanihiteshabc@gmail.com>
2024-08-21 18:02:45 +0530

    initial commit

---
+++ Trigr Project/Trigr_ProjectApp.swift
@@ -0,0 +1,17 @@
+//
+//  Trigr_ProjectApp.swift
+//  Trigr Project
+//
+//  Created by Hitesh Rupani on 21/08/24.
+//
+
+import SwiftUI
+
+@main
+struct Trigr_ProjectApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
